163a7bc1d7e97f02b81751b6ba915ed44ce4f3ae
timothy hartnett <tqh8pn@virginia.edu>
2021-08-16 15:44:45 -0400

    multiple updates 8-16-21

--- varying_experiments.py
+++ varying_experiments.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 15 15:06:46 2021
+Created on Fri Jul 30 13:46:25 2021
 
-@author: timhartnett
+state described by distribution mean predictions and variance predictions, training datasize and range
+
+positive rewards only
+
+@author: timothy
 """
 import pandas as pd #stable version 1.3.0
 import os
@@ -18,6 +22,7 @@ from collections import deque
 random.seed(42)
 WORKING_DIR = os.getcwd()
 data_df = pd.read_csv(WORKING_DIR+'/train.csv')
+
 data_df = data_df.drop('number_of_elements',axis=1)
 data_df = data_df[['mean_atomic_mass','std_atomic_mass','mean_fie',
                   'mean_atomic_radius','std_atomic_radius',
@@ -25,6 +30,7 @@ data_df = data_df[['mean_atomic_mass','std_atomic_mass','mean_fie',
                   'std_FusionHeat','mean_ThermalConductivity',
                   'std_ThermalConductivity','mean_Valence','entropy_Valence','std_Valence',
                   'critical_temp']]
+
 correlation = data_df.corr()
 #data_df = data_df.drop(['mean_Density','wtd_mean_Density','gmean_Density','wtd_mean_Density','std_Density',
 #                        'wtd_std_Density','range_Density','wtd_range_Density'],axis=1)
@@ -35,7 +41,7 @@ class GP_model(object):
     '''
     def __init__(self,training_data):
         self.training_data = training_data
-    def train_GP(self,kernel='Matern52',optimizer='L-BFGS-B'):
+    def train_GP(self,kernel='Matern32',optimizer='L-BFGS-B'):
         X = self.training_data[:,:-1]
         Y = self.training_data[:,-1].reshape(-1,1)
         X = (X - X.mean()) / X.std()
@@ -48,27 +54,11 @@ class GP_model(object):
 
 class environment(object):
     '''
-    Environment is described by normalized svalues of training value (y-mu)/sd. top 20 and bottom 20
+    Environment is described by normalized svalues of training value (y-mu)/sd.
     '''
     def __init__(self,virtual_data):
-        self.virtual_data = virtual_data
-    def state(self,current_data):
-        mean = np.mean(current_data[:,-1])
-        sd = np.std(current_data[:,-1])
-        norm_data = (current_data[:,-1]-mean)/sd
-        norm_data.sort()
-        state = np.concatenate((norm_data[:10],norm_data[-10:]))
-        return state
-    def reset(self, n_initial):
-        '''
-        this method is to generate a random initial training set for a toy problem and train the initial GP
-        '''
-        initial_selection = np.unique(random.sample(range(self.virtual_data.shape[0]),100))
-        self.current_data = self.virtual_data[initial_selection,:]
-        self.current_state = self.state(self.current_data)
-        self.virtual_data = np.delete(self.virtual_data,initial_selection,axis=0)
-        model = GP_model(self.current_data)
-        self.m = model.train_GP()
+        self.initial_data = virtual_data
+    
     def predict_virtual(self):
         X = self.virtual_data[:,:-1]
         X = (X - X.mean()) / X.std()
@@ -76,7 +66,36 @@ class environment(object):
         mean = norm_mean*self.current_data[:,-1].std()+self.current_data[:,-1].mean()
         var = norm_var*self.current_data[:,-1].std()+self.current_data[:,-1].mean()
         return mean, var
-    def step(self,action):
+    
+    def state(self,current_data,experiments_remaining):
+        mean = np.mean(current_data[:,-1])
+        sd = np.std(current_data[:,-1])
+        norm_data = (current_data[:,-1]-mean)/sd
+        norm_data.sort()
+        virtual_mean,virtual_var = self.predict_virtual()
+        distribution_means_mean = np.mean(virtual_mean-mean)
+        distribution_means_var = np.std(virtual_mean-mean)
+        distribution_var_mean = np.mean(virtual_var-sd)
+        distribution_var_var = np.std(virtual_var-sd)
+        state = np.array([norm_data[0],norm_data[-1],current_data.shape[0],experiments_remaining,
+                         distribution_means_mean, distribution_means_var, distribution_var_mean,
+                         distribution_var_var])
+        return state
+    
+    def reset(self, n_initial, n_exp):
+        '''
+        this method is to generate a random initial training set for a toy problem and train the initial GP
+        '''
+        self.virtual_data = self.initial_data.copy()
+        self.initial_n_exp = n_exp
+        initial_selection = np.unique(random.sample(range(self.virtual_data.shape[0]),100))
+        self.current_data = self.virtual_data[initial_selection,:]
+        self.virtual_data = np.delete(self.virtual_data,initial_selection,axis=0)
+        model = GP_model(self.current_data)
+        self.initial_max = np.max(self.current_data[:,-1])
+        self.m = model.train_GP()
+        self.current_state = self.state(self.current_data, n_exp)
+    def step(self,action,experiment_number):
         mean, var = self.predict_virtual()
         if action == 0:
             info = 'exploit'
@@ -90,13 +109,13 @@ class environment(object):
             info = 'random'
             new_data_index = random.sample(range(self.virtual_data.shape[0]),1)
             new_data = self.virtual_data[new_data_index,:]
-        if new_data[:,-1] > np.max(self.current_data[:,-1]):
-            reward = 1
+        if new_data[0,-1] > np.max(self.current_data[:,-1]):
+            reward = new_data[0,-1]-np.max(self.current_data[:,-1])
         else:
-            reward = -1
+            reward = 0
         self.current_data = np.unique(np.vstack((self.current_data,new_data)),axis=0)
         self.virtual_data = np.delete(self.virtual_data,new_data_index,axis=0)
-        new_state = self.state(self.current_data)
+        new_state = self.state(self.current_data, self.initial_n_exp-experiment_number)
         done = self.is_done()
         self.current_state = new_state
         return new_state, reward, done, info
@@ -174,11 +193,12 @@ actions = ['exploit','explore','random']
 epsilon = 1 # Epsilon-greedy algorithm in initialized at 1 meaning every step is random at the start
 max_epsilon = 1 # You can't explore more than 100% of the time
 min_epsilon = 0.01 # At a minimum, we'll always explore 1% of the time
-decay = 0.1
+decay = 0.01
+number_experiments = 15
 # 1. Initialize the Target and Main models
 # Main Model (updated every 4 steps)
 env = environment(data)
-env.reset(100)
+env.reset(20,number_experiments)
 main = agent(state_shape = env.current_state.shape, action_shape = num_actions,
               learning_rate = 0.7)
 main_model = main.DQN()
@@ -197,21 +217,22 @@ replay_memory = deque(maxlen=50_000)
 #y = []
 
 steps_to_update_target_model = 0
-total_steps = []
+total_increase_critical_temperature = []
 for episode in range(train_episodes):
     random.seed(42+episode*10)
     total_training_rewards = 0
-    #env.reset(100)
+    number_experiments = random.randint(10,100)
+    env.reset(random.randint(20,100),number_experiments)
+    mean_1, var_1 = env.predict_virtual()
+    print(mean_1[:10])
+    print(var_1[:10])
     observation = env.current_state
     done = False
-    i = 0
-    while not done:
-        print('EPISODE' + str(episode))
-        print('epsilon = '+str(epsilon))
+    for i in range(number_experiments):
+        print('EPISODE ' + str(episode))
+        print('experiment ' + str(i))
         steps_to_update_target_model += 1
         random_number = np.random.rand()
-        print('number of training samples'+str(env.current_data.shape))
-        i += 1
         if random_number <= epsilon:
             # Explore
             print('epsilon action')
@@ -224,32 +245,76 @@ for episode in range(train_episodes):
             predicted = main_model.predict(encoded_reshaped).flatten()
             print('predicted q values = ' + str(predicted))
             action = np.argmax(predicted)
-        new_observation, reward, done, info = env.step(action)
+        new_observation, reward, done, info = env.step(action,i)
+        print('reward = '+str(reward))
         replay_memory.append([observation, action, reward, new_observation, done])
 
         # 3. Update the Main Network using the Bellman Equation
-        if steps_to_update_target_model % 10 == 0 or done:
+        if steps_to_update_target_model % 5 == 0 or done:
+            print('#### Updating main DQN ####')
             train(env, replay_memory, main_model, target_model, done)
 
         observation = new_observation
         total_training_rewards += reward
         env.update_model()
-        print_summary(env.m)
-        print('distance to max')
-        print(np.max(env.virtual_data[:,-1])-np.max(env.current_data[:,-1]))
+        print('max critical Temp')
+        print(np.max(env.current_data[:,-1]))
         print('action = '+actions[action])     
         if done:
             print('Total training rewards: {} after n steps = {} with final reward = {}'.format(total_training_rewards, episode, reward))
-            total_training_rewards += 1
-
-            if steps_to_update_target_model >= 100:
-                print('Copying main network weights to the target network weights')
-                target_model.set_weights(main_model.get_weights())
-                steps_to_update_target_model = 0
-            total_steps.append(i)
+            total_training_rewards += 100
             break
-        if i == 10000:
-            break
-    epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * episode)
-
         
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * episode)
+        
+    total_increase_critical_temperature.append(env.initial_max-np.max(env.current_data[:,-1]))
+    
+    if steps_to_update_target_model >= 100:
+        print('Copying main network weights to the target network weights')
+        target_model.set_weights(main_model.get_weights())
+        steps_to_update_target_model = 0
+
+# serialize model to JSON
+target_json = target_model.to_json()
+with open("model_varying_experiments_1.json", "w") as json_file:
+    json_file.write(target_json)
+# serialize weights to HDF5
+target_model.save_weights("model_varying_experiments_1.h5")
+print("Saved model to disk")
+
+# load json and create model
+json_file = open('model_varying_experiments_1.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model_3 = tf.keras.models.model_from_json(loaded_model_json)
+
+model_3.load_weights("model_varying_experiments_1.h5")
+print("Loaded model from disk")
+
+'''TEST RUNS '''
+Test_runs = 100
+actions = ['exploit','explore','random']
+improvement_tracker = []
+env = environment(data)
+for episode in range(Test_runs):
+    print(str(episode))
+    random.seed(42+episode*10)
+    number_experiments = random.randint(10,100)
+    total_training_rewards = 0
+    env.reset(random.randint(20,100),number_experiments)
+    observation = env.current_state
+    done = False
+    for i in range(number_experiments):
+        encoded = encode_observation(observation, observation.shape[0])
+        encoded_reshaped = encoded.reshape([1, encoded.shape[0]])
+        predicted = model_3.predict(encoded_reshaped).flatten()
+        action = np.argmax(predicted)
+        new_observation, reward, done, info = env.step(action,i)
+        observation = new_observation
+        total_training_rewards += reward
+        env.update_model()
+        
+    improvement_tracker.append(np.max(env.current_data[:,-1])-env.initial_max)
+
+improvement = np.array(improvement_tracker)
+improvement = pd.DataFrame(improvement.astype('float'),columns = ['Increase_T_c']).to_csv('model_4_test.csv')
